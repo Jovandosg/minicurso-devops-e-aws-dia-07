727bb199fb79bc93025dbadc1a01e0c5d7fdf551
Kenerry Serain <38140596+kenerry-serain@users.noreply.github.com>
2025-06-06 20:31:50 -0300

    Uploading

---
+++ terraform/main-stack/iam.role.github.tf
@@ -0,0 +1,80 @@
+resource "aws_iam_role" "github" {
+  name = "mini-curso-devops-aws-github-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        },
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:kenerry-serain/minicurso-devops-e-aws-dia-07:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "github" {
+  name        = "github-actions-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "GetAuthorizationToken"
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowPushPull"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+        Resource = aws_ecr_repository.this[*].arn
+      },
+      {
+        Sid    = "AllowUploadFiles"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Resource = [
+          aws_s3_bucket.this.arn,
+          "${aws_s3_bucket.this.arn}/*"
+        ]
+      },
+      {
+        Sid    = "AllowInvalidation"
+        Effect = "Allow"
+        Action = [
+          "cloudfront:CreateInvalidation",
+        ]
+        Resource = [
+          aws_cloudfront_distribution.this.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_custom_policy" {
+  policy_arn = aws_iam_policy.github.arn
+  role       = aws_iam_role.github.name
+}
